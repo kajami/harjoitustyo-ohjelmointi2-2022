9858d43f87a9f66aadf46469a60c0dc81bdf1140
kajami <mikko.kajaluoto@gmail.com>
2022-05-07 16:03:21 +0300

    some code formatting

--- src/main/java/database/AlbumDao.java
+++ src/main/java/database/AlbumDao.java
@@ -14,34 +14,34 @@ public class AlbumDao {
     private final Database db = new Database();
 	
 	public List<Album> getAllAlbums(long id){
-	 	Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet results = null;
-
-        List<Album> albums = new ArrayList<>();
-
-        try {
-            connection = this.db.connect();
-            
-            statement = connection.prepareStatement("SELECT AlbumId, Title, ArtistId FROM Album WHERE ArtistId = ?");
-            statement.setLong(1, id);
-            results = statement.executeQuery();
-
-            while (results.next()) {
-                long AlbumId = results.getLong("AlbumId");
-                String title = results.getString("Title");
-
-                Album newAlbum = new Album(AlbumId, title);
-
-                albums.add(newAlbum);
-            }
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            this.db.close(connection, statement, results);
-        }
-        return albums;
-    }
+		 	Connection connection = null;
+	        PreparedStatement statement = null;
+	        ResultSet results = null;
+	
+	        List<Album> albums = new ArrayList<>();
+	
+	        try {
+	            connection = this.db.connect();
+	            
+	            statement = connection.prepareStatement("SELECT AlbumId, Title, ArtistId FROM Album WHERE ArtistId = ?");
+	            statement.setLong(1, id);
+	            results = statement.executeQuery();
+	
+	            while (results.next()) {
+	                long AlbumId = results.getLong("AlbumId");
+	                String title = results.getString("Title");
+	
+	                Album newAlbum = new Album(AlbumId, title);
+	
+	                albums.add(newAlbum);
+	            }
+	            
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } finally {
+	            this.db.close(connection, statement, results);
+	        }
+	        return albums;
+	    }
 
 }
